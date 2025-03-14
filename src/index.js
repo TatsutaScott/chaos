bec2aae85cb9188490ca6e7b56d80e5b54104405
TatsutaScott <tatsuta.scott@gmail.com>
2025-03-14 14:39:03 +0900

    reorganized code so all vis can be done in separate p5 sketches

--- src/index.js
+++ src/index.js
@@ -1,139 +1,285 @@
 window.p5 = require("p5");
 require("p5/lib/addons/p5.sound");
 import { createDevice, TimeNow, MessageEvent } from "@rnbo/js";
-import patcher from "./export/chaos_v3.export.json";
-import { Bubble, AnimationQueue } from "./animation";
-import {
-  cursor,
-  point_plot,
-  amp_plot,
-  dist_plot,
-  details,
-  info,
-  extra_info,
-} from "./ui_elements";
+import patcher from "./export/chaos_v3.3.export.json";
+import ScrollingWaveform from "./vis/ScrollingWaveform";
+import Spectrogram from "./vis/Spectrogram";
+import InstantSpectrum from "./vis/Spectrum";
+import distGraph from "./vis/distGraph";
+import InstantWaveform from "./vis/InstantWaveform";
+import pointPlot from "./vis/pointPlot";
+import cursor from "./vis/cursor";
+import { Bubble, AnimationQueue } from "./vis/animation";
 
-import AudioVisCollection from "./audioVisualization/AudioVis";
-
-let queue,
-  song,
-  context,
+let context,
   outputNode,
   device,
   point_arr,
   dist_arr,
-  master = 1,
-  AV,
-  amp_arr = [];
+  master = false,
+  waveform,
+  spectrum,
+  amp_arr = [],
+  fft = new p5.FFT(0, 128);
 
-new p5((p) => {
-  const colors = {
-    bg: p.color(200),
-    fg: p.color(50),
-    i: p.color(50, 50, 50, 50),
-  };
+window.addEventListener("mousemove", (event) => {
+  try {
+    setParam("x", event.clientX / window.innerWidth);
+    setParam("y", event.clientY / window.innerHeight);
+  } catch (err) {
+    console.log(err);
+  }
+});
+window.addEventListener("touchmove", (event) => {
+  try {
+    setParam("x", event.changedTouches[0].clientX / window.innerWidth);
+    setParam("y", event.changedTouches[0].clientY / window.innerHeight);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-  p.preload = () => {
+window.addEventListener("pointerdown", () => {
+  try {
+    setParam("left_click", 1);
+    setParam("left_click", 0);
+    if (!master) {
+      master = true;
+      setParam("master", 1);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.code == "Space") {
+    master = !master;
+    const vol = master ? 1 : 0;
+    setParam("master", vol);
+  }
+});
+
+const colors = {
+  bg: 200,
+  fg: 50,
+  i: 50,
+};
+
+//INSTANT WAVE
+const instant_wave_container = document.getElementById("instant-wave-cnv");
+const instant_wave_sketch = (p) => {
+  let instant_wave;
+  p.setup = () => {
+    const instant_wave_p5 = p.createCanvas(
+      instant_wave_container.clientWidth,
+      instant_wave_container.clientHeight
+    );
+    instant_wave_p5.parent(instant_wave_container);
+
     rnbo_setup(p.getAudioContext()).then((e) => {
-      AV.setFFTInput(outputNode);
+      fft.setInput(outputNode);
+      setParam("left_click", 1);
+      setParam("left_click", 0);
     });
+
+    instant_wave = new InstantWaveform(p, colors);
   };
+
+  p.draw = () => {
+    waveform = fft.waveform();
+    spectrum = fft.analyze();
+
+    if (waveform) {
+      instant_wave.update(waveform);
+      instant_wave.draw();
+    }
+  };
+};
+
+//INSTANT SPECTRUM
+const instant_spec_container = document.getElementById("instant-spectrum-cnv");
+const instant_spec_sketch = (p) => {
+  let instant_spec;
 
   p.setup = () => {
-    p.createCanvas(window.innerWidth, window.innerHeight);
-    AV = new AudioVisCollection(
-      p,
-      p.width * 0.15,
-      0,
-      p.width * 0.85,
-      p.width * 0.15,
-      5,
-      colors
+    const instant_spec_p5 = p.createCanvas(
+      instant_spec_container.clientWidth,
+      instant_spec_container.clientHeight
     );
-    p.textFont("Courier New", 10);
+    instant_spec_p5.parent(instant_spec_container);
+
+    instant_spec = new InstantSpectrum(p, colors);
+  };
+
+  p.draw = () => {
+    if (spectrum) {
+      instant_spec.update(spectrum);
+      instant_spec.draw();
+    }
+  };
+};
+
+//POINT PLOT
+const point_plot_container = document.getElementById("point-plot-cnv");
+const point_plot_sketch = (p) => {
+  p.setup = () => {
+    const plot_point_p5 = p.createCanvas(
+      point_plot_container.clientWidth,
+      point_plot_container.clientHeight
+    );
+    plot_point_p5.parent(point_plot_container);
+
+    let resizeObserver = new ResizeObserver(() => {
+      p.resizeCanvas(
+        point_plot_container.clientWidth,
+        point_plot_container.clientHeight
+      );
+    });
+    resizeObserver.observe(point_plot_container);
+    p.textFont("monospace");
+  };
+
+  p.draw = () => {
+    pointPlot(p, colors, point_arr);
+  };
+};
+
+//DIST GRAPH
+const distance_graph_container = document.getElementById("distance-graph-cnv");
+const distance_graph_sketch = (p) => {
+  p.setup = () => {
+    const dist_graph_p5 = p.createCanvas(
+      distance_graph_container.clientWidth,
+      distance_graph_container.clientHeight
+    );
+    dist_graph_p5.parent(distance_graph_container);
+    let resizeObserver = new ResizeObserver(() => {
+      p.resizeCanvas(
+        distance_graph_container.clientWidth,
+        distance_graph_container.clientHeight
+      );
+    });
+    resizeObserver.observe(distance_graph_container);
+  };
+
+  p.draw = () => {
+    distGraph(p, colors, dist_arr);
+  };
+};
+
+//SCROLLING SPECTRUM
+const scrolling_spec_container = document.getElementById(
+  "scrolling-spectrum-cnv"
+);
+const scroll_spec_sketch = (p) => {
+  let scrolling_wave, scrolling_spec;
+
+  p.setup = () => {
+    const scrolling_spec_p5 = p.createCanvas(
+      scrolling_spec_container.clientWidth,
+      scrolling_spec_container.clientHeight
+    );
+    scrolling_spec_p5.parent(scrolling_spec_container);
+    scrolling_wave = new ScrollingWaveform(p, colors);
+    scrolling_spec = new Spectrogram(p, colors);
+  };
+
+  p.draw = () => {
+    p.clear();
+    scrolling_wave.update(waveform);
+    scrolling_spec.update(spectrum);
+
+    scrolling_spec.draw();
+    scrolling_wave.draw();
+  };
+};
+
+//POINT DATA
+const point_data_container = document.getElementById("data-container");
+let data_dom;
+const update_data = () => {
+  if (dist_arr) {
+    const short = window.innerWidth < 700;
+    if (!data_dom) {
+      data_dom = [];
+
+      for (let i = 0; i < dist_arr.length; i++) {
+        if (point_arr[i * 2] && point_arr[i * 2 + 1]) {
+          //   const point_string = `#${pad(i, 3)}:[${point_arr[i * 2].toFixed(
+          //     2
+          //   )}, ${point_arr[i * 2 + 1].toFixed(2)}] ${dist_arr[i].toFixed(2)}`;
+
+          const data_point = document.createElement("span");
+          data_point.classList.add("data");
+          data_point.innerHTML = data_string(
+            i,
+            point_arr[i * 2],
+            point_arr[i * 2 + 1],
+            dist_arr[i],
+            short
+          );
+          data_dom.push(data_point);
+          point_data_container.appendChild(data_point);
+        }
+      }
+    } else {
+      for (let i = 0; i < data_dom.length; i++) {
+        data_dom[i].innerHTML = data_string(
+          i,
+          point_arr[i * 2],
+          point_arr[i * 2 + 1],
+          dist_arr[i],
+          short
+        );
+      }
+    }
+  }
+};
+
+const data_string = (index, x, y, d, short) => {
+  if (short) {
+    return `#${pad(index, 3)}: ${d.toFixed(2)}`;
+  } else {
+    return `#${pad(index, 3)}:[${x.toFixed(2)}, ${y.toFixed(2)}] ${d.toFixed(
+      2
+    )}`;
+  }
+};
+window.addEventListener("mousemove", update_data);
+window.addEventListener("touchmove", update_data);
+
+function pad(num, size) {
+  num = num.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
+}
+
+//OVERLAY
+const overlay_container = document.getElementById("overlay");
+const overlay_sketch = (p) => {
+  let queue;
+  p.setup = () => {
+    const overlay_p5 = p.createCanvas(
+      overlay_container.clientWidth,
+      overlay_container.clientHeight
+    );
+    overlay_p5.parent(overlay_container);
     queue = new AnimationQueue();
   };
 
   p.draw = () => {
-    p.background(200);
+    p.clear();
+    cursor(p, colors);
     queue.update(p);
-    //  ui(p, 150, 200, point_arr, dist_arr, amp_arr, false);
-    try {
-      setParam("x", p.mouseX / p.width);
-      setParam("y", p.mouseY / p.height);
-    } catch (err) {
-      console.log(err);
-    }
-
-    AV.update();
-    AV.draw();
-
-    point_plot(
-      { p5: p, dark: 50, light: 200, dev: false },
-      point_arr,
-      0,
-      0,
-      300,
-      300,
-      20
-    );
   };
 
   p.mousePressed = () => {
-    setParam("left_click", 1);
-    setParam("left_click", 0);
+    if (queue) {
+      queue.add(new Bubble(p.mouseX, p.mouseY));
+    }
   };
-});
-
-// new p5((p) => {
-//   p.preload = async () => {
-//     rnbo_setup();
-//   };
-
-//   p.setup = () => {
-//     queue = new AnimationQueue();
-//     p.createCanvas(window.innerWidth, window.innerHeight);
-//     p.textFont("Courier New", 10);
-//     console.log(p);
-//   };
-
-//   p.draw = () => {
-//     p.background(200);
-//     queue.update(p);
-
-//     try {
-//       setParam("x", p.mouseX / p.width);
-//       setParam("y", p.mouseY / p.height);
-//       // setParam("master", 1);
-//       // sendMessage("get_amp", "bang");
-//     } catch (err) {
-//       console.log(err);
-//     }
-
-//     ui(p, 150, 200, point_arr, dist_arr, amp_arr, false);
-//   };
-
-//   p.mousePressed = () => {
-//     queue.add(new Bubble(p.mouseX, p.mouseY));
-//     setParam("left_click", 1);
-//     setParam("left_click", 0);
-//   };
-
-//   p.keyPressed = (v) => {
-//     //mute / unmute on space bar
-//     if (v.keyCode === 32) {
-//       if (master == 0) {
-//         master = 1;
-//         setParam("master", 1);
-//       } else {
-//         master = 0;
-//         setParam("master", 0);
-//       }
-//     } else if (v.keyCode === 82) {
-//       rnbo_setup();
-//     }
-//   };
-// });
+};
 
 function setParam(param, value) {
   const p = device.parametersById.get(param);
@@ -145,46 +291,14 @@ function sendMessage(tag, message) {
   device.scheduleEvent(event1);
 }
 
-// all of the ui components
-function ui(p5, dark, light, points, dists, amps, dev) {
-  const h = p5.height;
-  const w = p5.width;
-  const settings = { p5, dark, light, dev };
-  const left_w = h * 0.8; //width of the left column
-
-  try {
-    //left hand column
-    point_plot(settings, points, 0, 0, left_w, left_w, 20);
-    amp_plot(settings, amps, 0, left_w, left_w, h - left_w, 20);
-
-    //right hand column
-    details(settings, points, dists, 5, left_w, 0, w - left_w, h / 2, 20);
-    dist_plot(settings, dists, left_w, h / 2, w - left_w, h / 4, 20);
-    extra_info(
-      settings,
-      points,
-      dists,
-      amps,
-      left_w,
-      h * 0.75,
-      (w - left_w) / 2,
-      h / 4,
-      20
-    );
-  } catch (e) {
-    console.log(e);
-  }
-
-  info(
-    settings,
-    left_w + (w - left_w) / 2,
-    h * 0.75,
-    (w - left_w) / 2,
-    h / 4,
-    20
-  );
-  cursor(settings);
-}
+window.addEventListener("load", () => {
+  new p5(instant_wave_sketch);
+  new p5(point_plot_sketch);
+  new p5(instant_spec_sketch);
+  new p5(scroll_spec_sketch);
+  new p5(distance_graph_sketch);
+  new p5(overlay_sketch);
+});
 
 //set up the rnbo device. used on initialization and on reset
 async function rnbo_setup(ctx) {
@@ -218,7 +332,6 @@ async function rnbo_setup(ctx) {
         }
         break;
       default:
-      //console.log(e);
     }
   });
 }
